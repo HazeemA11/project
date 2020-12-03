feda70c4309a5e6de63e4c0cf3f5f0e613d2ebea
HazeemA11 <71974103+HazeemA11@users.noreply.github.com>
2020-12-02 19:45:26 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,35 @@
-
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-var bobObject1, bobObject2, bobObject3, bobObjec4, bobObject5,rope;
-
-function preload()
-{
-	
-}
-
-function setup() {
-	createCanvas(800, 700);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	rope=new rope (bobObject1.body,roofObject.body,-bobDiameter*2,0)
-
-
-	Engine.run(engine);
-
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
+var bullet,wall;
+var speed,weight;
+var object2, movingRect;
+ 
+function setup(){
+  createCanvas(1600,800);
   
-  drawSprites();
+  speed=random(55,90);
+  weight=random(400,1500)
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,30,height/2);
+  car.velocityX=speed;
+}
+ 
+function draw() {
+  background(0,0,0);  
+
+ 
+if(wall.x-car.x<(car.width+wall.width)/2){
+ 
+car.velocityX=0;
+var deformation=0.5*weight*speed*speed/22509;
+if(deformation<180){
+  car.shapeColor=color(255,0,0);
+}
+if(deformation<180&&deformation>100){
+car.shapeColor=color(230,230,0)
+}
+if(deformation<100){
+  car.shapeColor=color(0,255,0)
+}
  
 }
-
-
-
+drawSprites();
+}
